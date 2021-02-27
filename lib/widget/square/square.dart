bafbc998bd456d8dc14ac28e029d19645d94ceb7
Kane.Li <Kane.Li@movit-tech.com>
2021-02-27 21:25:04 +0800

    魔方组件调整

--- lib/widget/square/square.dart
+++ lib/widget/square/square.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:zk/constant/constant.dart';
+import 'package:zk/util/screen_util.dart';
 
 class SquareWidget extends StatelessWidget {
   @override
@@ -7,33 +9,55 @@ class SquareWidget extends StatelessWidget {
     return Padding(
         padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
       child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Expanded(
-              flex: 1,
-              child: Container(
-                height: 140,
-                child: Image.network(Constant.TestImage),
-              )),
+          // Expanded(
+          //     flex: 1,
+          //     child: Container(
+          //       height: 140,
+          //       child: Image.network(Constant.TestImage),
+          //     )),
+          // SizedBox(width: 10),
+          // Expanded(
+          //     flex: 1,
+          //     child: Container(
+          //         height: 140,
+          //         child: Image.network(Constant.TestImage))),
+          // SizedBox(width: 10),
+          // Expanded(
+          //     flex: 1,
+          //     child: Container(
+          //       height: 140,
+          //       child: Image.network(Constant.TestImage),
+          //     )),
+          // SizedBox(width: 10),
+          // Expanded(
+          //     flex: 1,
+          //     child: Container(
+          //       height: 140,
+          //       child: Image.network(Constant.TestImage),
+          //     )),
+          Container(
+            color: Colors.green,
+            width: (ScreenUtil.screenWidthDp/4-13),
+            height: 100,
+            child: Image.network(Constant.TestImage),
+          ),
           SizedBox(width: 10),
-          Expanded(
-              flex: 1,
-              child: Container(
-                  height: 140,
-                  child: Image.network(Constant.TestImage))),
+          Container(
+            width: (ScreenUtil.screenWidthDp/4-13),
+            child: Image.network(Constant.TestImage),
+          ),
           SizedBox(width: 10),
-          Expanded(
-              flex: 1,
-              child: Container(
-                height: 140,
-                child: Image.network(Constant.TestImage),
-              )),
+          Container(
+            width: (ScreenUtil.screenWidthDp/4-13),
+            child: Image.network(Constant.TestImage),
+          ),
           SizedBox(width: 10),
-          Expanded(
-              flex: 1,
-              child: Container(
-                height: 140,
-                child: Image.network(Constant.TestImage),
-              )),
+          Container(
+            width: (ScreenUtil.screenWidthDp/4-13),
+            child: Image.network(Constant.TestImage),
+          ),
         ],
       )
     );
